0d79fa880f645a6109c1080204e64eaeb419f77f
erik streller <erik.lilp@gmail.com>
2022-04-06 23:33:37 +0200

    feat: add page container

---
+++ components/PageContainer.tsx
@@ -0,0 +1,76 @@
+import clsx from 'clsx'
+import { useTheme } from 'next-themes'
+import Head from 'next/head'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { PropsWithChildren, useEffect, useState } from 'react'
+import { FiMoon, FiSun } from 'react-icons/fi'
+
+function NavItem({ href, text }: { href: string; text: string }) {
+  const router = useRouter()
+  const isActive = router.asPath === href
+
+  return (
+    <Link href={href}>
+      <a
+        className={clsx(
+          isActive
+            ? 'font-bold text-gray-800 dark:text-gray-200'
+            : 'rounded-lg px-3 py-2 font-normal text-gray-600 transition-all hover:bg-gray-200 dark:text-gray-400 dark:hover:bg-gray-800'
+        )}
+      >
+        {text}
+      </a>
+    </Link>
+  )
+}
+
+const customMeta = {
+  title: 'Erik Streller',
+  description: '',
+  type: 'website'
+}
+
+type PageContainerProps = {
+  page?: string
+} & Partial<typeof customMeta>
+
+export default function PageContainer(
+  props: PropsWithChildren<PageContainerProps>
+) {
+  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState<boolean>(false)
+
+  const meta = { ...customMeta, ...props }
+
+  meta['title'] = props.page ? `${props.page} - Erik Streller` : meta.title
+
+  useEffect(() => setMounted(true), [])
+
+  return (
+    <div>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name='description' content={meta.description} />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className='layout'>
+        <nav className='flex items-center justify-between py-8'>
+          <div className='space-x-6'>
+            <NavItem href='/' text='Home' />
+            <NavItem href='/blog' text='Blog' />
+            <NavItem href='/colophon' text='Colophon' />
+          </div>
+          <button
+            className='flex h-9 w-9 items-center justify-center rounded-lg bg-gray-200 ring-gray-400 transition-all hover:ring-2 dark:bg-gray-800'
+            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          >
+            {mounted &&
+              (theme === 'light' ? <FiMoon size={20} /> : <FiSun size={20} />)}
+          </button>
+        </nav>
+      </div>
+      <main className='layout'>{props.children}</main>
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const { fontFamily } = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
