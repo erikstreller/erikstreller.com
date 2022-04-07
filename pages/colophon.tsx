55549fa09788315aa2b10760bfde417436900c27
erik streller <erik.lilp@gmail.com>
2022-04-07 19:22:58 +0200

    feat: add colophon page

---
+++ pages/colophon.tsx
@@ -0,0 +1,61 @@
+import NewTab from '@/components/NewTab'
+import PageContainer from '@/components/PageContainer'
+
+export default function colophon() {
+  return (
+    <PageContainer page='Colophon'>
+      <div className='section-spacing'>
+        <h1 className='page-heading'>Colophon.</h1>
+        <p className='page-description'>
+          Designed and developed by Erik Streller.
+        </p>
+        <p className='page-description'>
+          This website uses{' '}
+          <NewTab href={links.next} className={linkColor}>
+            Next.js
+          </NewTab>{' '}
+          as a framework. All pages are statically generated and styled with{' '}
+          <NewTab href={links.tailwind} className={linkColor}>
+            TailwindCSS
+          </NewTab>
+          . For the design a combination of{' '}
+          <NewTab href={links.excalidraw} className={linkColor}>
+            Excalidraw
+          </NewTab>{' '}
+          and{' '}
+          <NewTab href={links.figma} className={linkColor}>
+            Figma
+          </NewTab>{' '}
+          is used.
+        </p>
+        <p className='page-description'>
+          It is deployed on{' '}
+          <NewTab href={links.vercel} className={linkColor}>
+            Vercel
+          </NewTab>{' '}
+          and the{' '}
+          <NewTab href={links.source} className='text-blue-500'>
+            Source Code
+          </NewTab>{' '}
+          can be found on{' '}
+          <NewTab href={links.github} className={linkColor}>
+            GitHub
+          </NewTab>
+          .
+        </p>
+      </div>
+    </PageContainer>
+  )
+}
+
+const links = {
+  next: 'https://nextjs.org/',
+  tailwind: 'https://tailwindcss.com/',
+  excalidraw: 'https://excalidraw.com/',
+  figma: 'https://figma.com/',
+  vercel: 'https://vercel.com/',
+  github: 'https://github.com/',
+  source: 'https://github.com/erikstreller/new-me'
+}
+
+const linkColor = 'text-dark dark:text-light'
