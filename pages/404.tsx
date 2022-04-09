253abfb611740dbe3522a2b7978e717d54e41a85
erik streller <erik.lilp@gmail.com>
2022-04-09 13:50:29 +0200

    feat: add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,17 @@
+import PageContainer from '@/components/PageContainer'
+
+export default function Custom404() {
+  return (
+    <PageContainer page='404'>
+      <div className='section-spacing pb-80'>
+        <h1 className='page-heading'>
+          You found the <span className='text-teal-400'>404</span> page.
+        </h1>
+        <p className='page-description'>
+          Was there something you where looking for? One reason could be it
+          doesn't exist, at least not anymore.
+        </p>
+      </div>
+    </PageContainer>
+  )
+}
