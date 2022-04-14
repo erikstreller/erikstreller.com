7c63394b94f9e00e7cc9b2bb0b9eca3b6c655c31
erik streller <erik.lilp@gmail.com>
2022-04-14 16:08:58 +0200

    feat: add color theme to home page

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import { FiArrowUpRight } from 'react-icons/fi'
 import {
   SiChakraui,
@@ -12,16 +13,28 @@ export default function Projects() {
   return (
     <div className='section-spacing'>
       <h3 className='section-heading'>Projects</h3>
-      <p className='section-description mb-8'>Learn – Practice – Repeat</p>
-      <div className='flex flex-col space-y-8'>
+      <p className='section-description'>Learn – Practice – Repeat</p>
+      <div className='flex flex-col gap-6 pt-6'>
         {projects.map((project, id) => (
-          <NewTab key={id} href={project.link} className='group'>
-            <div className='grid grid-cols-8 items-center gap-2 text-lg transition-all hover:scale-[1.01] md:text-xl'>
+          <NewTab
+            key={id}
+            href={project.link}
+            className={clsx(
+              'group w-full rounded-xl p-[2px] transition',
+              'blue-card-color bg-gradient-to-bl hover:scale-[1.01]'
+            )}
+          >
+            <div
+              className={clsx(
+                'grid grid-cols-8 items-center gap-2 text-lg',
+                'rounded-[10px] bg-white py-2 px-3 dark:bg-dark'
+              )}
+            >
               <div className='col-span-1 text-slate-400 dark:text-gray-500'>
                 {project.number}
               </div>
               <div className='col-span-6 flex items-center text-slate-900 dark:text-light'>
-                <h4>{project.name}</h4>
+                <h4 className='font-medium tracking-tight'>{project.name}</h4>
                 <FiArrowUpRight className='ml-2' />
               </div>
               <div className='col-span-1 flex space-x-4 justify-self-end'>
