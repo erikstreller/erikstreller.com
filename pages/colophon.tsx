b39444fd319d8ddebfa36c95cbb5e6213414eb6f
erik streller <erik.lilp@gmail.com>
2022-04-15 13:19:06 +0200

    feat: update tech stack

--- pages/colophon.tsx
+++ pages/colophon.tsx
@@ -18,7 +18,23 @@ export default function colophon() {
           <NewTab href={links.tailwind} className={linkColor}>
             TailwindCSS
           </NewTab>
-          . For the design a combination of{' '}
+          . Blog posts are written in{' '}
+          <NewTab href={links.mdx} className={linkColor}>
+            MDX
+          </NewTab>{' '}
+          (currently in dev mode) and implemented via{' '}
+          <NewTab href={links.contentlayer} className={linkColor}>
+            Contenlayer
+          </NewTab>
+          . The database will soon be{' '}
+          <NewTab href={links.planetscale} className={linkColor}>
+            PlanetScale
+          </NewTab>{' '}
+          with{' '}
+          <NewTab href={links.prisma} className={linkColor}>
+            Prisma
+          </NewTab>{' '}
+          as ORM. For the design a combination of{' '}
           <NewTab href={links.excalidraw} className={linkColor}>
             Excalidraw
           </NewTab>{' '}
@@ -55,6 +71,10 @@ const links = {
   figma: 'https://figma.com/',
   vercel: 'https://vercel.com/',
   github: 'https://github.com/',
+  planetscale: 'https://planetscale.com/',
+  prisma: 'https://prisma.io/',
+  mdx: 'https://mdxjs.com/',
+  contentlayer: 'https://contentlayer.dev/',
   source: 'https://github.com/erikstreller/erikstreller.com'
 }
 
