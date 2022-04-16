0d3bc29f93ce6030fe69612699efffa34dbbf04a
erik streller <erik.lilp@gmail.com>
2022-04-17 00:15:42 +0200

    feat: add avatar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx'
+import Image from 'next/image'
 import { IoNewspaperSharp } from 'react-icons/io5'
 import { SiGithub } from 'react-icons/si'
 import Accent from './Accent'
@@ -18,26 +19,39 @@ export default function Hero() {
   }
 
   return (
-    <div className='section-spacing'>
-      <h1 className='page-heading'>{headingColor()}</h1>
-      <p className='page-description max-w-md'>
-        Mechanical Engineer – engineering not only gears and plants. Exploring
-        web-development, learning technologies {'&'} building projects.
-      </p>
-      <div className='flex space-x-5 text-sm md:text-base'>
-        {links.map((link, id) => (
-          <NewTab
-            key={id}
-            href={link.link}
-            className={clsx(
-              'flex items-center space-x-2 transition-colors',
-              'text-slate-400 hover:text-black dark:text-gray-500 dark:hover:text-light'
-            )}
-          >
-            {link.icon}
-            <span>{link.name}</span>
-          </NewTab>
-        ))}
+    <div className='section-spacing flex flex-col-reverse sm:flex-row sm:justify-between'>
+      <div className='flex flex-col'>
+        <h1 className='page-heading'>{headingColor()}</h1>
+        <p className='page-description sm:max-w-[376px] md:max-w-md'>
+          Mechanical Engineer – engineering not only gears and plants. Exploring
+          web-development, learning technologies {'&'} building projects.
+        </p>
+        <div className='flex space-x-5 text-sm md:text-base'>
+          {links.map((link, id) => (
+            <NewTab
+              key={id}
+              href={link.link}
+              className={clsx(
+                'flex items-center space-x-2 transition-colors',
+                'text-slate-400 hover:text-black dark:text-gray-500 dark:hover:text-light'
+              )}
+            >
+              {link.icon}
+              <span>{link.name}</span>
+            </NewTab>
+          ))}
+        </div>
+      </div>
+      <div className='mb-[28px] w-[100px] md:w-auto'>
+        <Image
+          alt='Erik Streller'
+          width={125}
+          height={125}
+          quality={100}
+          src='/static/images/avatar.png'
+          priority
+          className='brightness-105'
+        />
       </div>
     </div>
   )
