9f248173163d63c07ff5468cdfdf49fd20bfa0d7
Dana Kuban <erik.lilp@gmail.com>
2022-05-27 23:37:03 +0100

    feat: add cypress to cv

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -48,7 +48,7 @@ const links = [
   {
     icon: <IoNewspaperSharp />,
     name: 'Resume',
-    link: 'https://drive.google.com/file/d/1UCQrl1Bcou2CcJgeQtLUWwHMe9m9Gs2o/view?usp=sharing'
+    link: 'https://drive.google.com/file/d/1HDKvDmOE0kh00vJqId8sFFXsgB-EbGCi/view?usp=sharing'
   },
   {
     icon: <SiGithub />,
